49d5a2de31519ee034241fd382097efb43ea4248
Luis Mauricio <Gah7583@gmail.com>
2022-07-19 18:42:24 -0300

    Melhorando a Tela

--- Xadrez/Tela.cs
+++ Xadrez/Tela.cs
@@ -6,11 +6,12 @@ internal class Tela
     {
         for (int i = 0; i < tab.Linhas; i++)
         {
+            Console.Write(8 - i + " ");
             for (int j = 0; j < tab.Colunas; j++)
             {
                 if (tab.Peca(i, j) is not null)
                 {
-                    Console.Write(tab.Peca(i, j) + " ");
+                    ImprimirPeca(tab.Peca(i, j));
                 }
                 else
                 {
@@ -18,6 +19,21 @@ internal class Tela
                 }
             }
             Console.WriteLine();
+        }
+        Console.WriteLine("  a b c d e f g h");
+    }
+    public static void ImprimirPeca(Peca peca)
+    {
+        if (peca.Cor == Cor.Branca)
+        {
+            Console.Write(peca + " ");
+        }
+        else
+        {
+            ConsoleColor aux = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(peca + " ");
+            Console.ForegroundColor = aux;
         }
     }
 }
